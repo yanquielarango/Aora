ebe22b6aa55a6add428c387eb67c6ef7532bdaaf
yanquielarango <yanquiel.arango@gmail.com>
2024-04-13 21:14:16 +0200

    creating welcome screen

--- app/index.jsx
+++ app/index.jsx
@@ -1,23 +1,14 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
+    <SafeAreaView className="bg-primary h-full">
 
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   )
 }
 
 
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      }
-})
